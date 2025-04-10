4bed802d8c12fd73435af902ea842281880305ae
Nidhin Roy <83805258+NidhinRoy@users.noreply.github.com>
2025-04-10 08:17:59 +0530

    new

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -3,29 +3,16 @@ import cv2
 def read_video(video_path):
     cap = cv2.VideoCapture(video_path)
     frames = []
-    
     while True:
         ret, frame = cap.read()
         if not ret:
             break
         frames.append(frame)
-    
-    cap.release()  # Release the video capture object
-    return frames  
+    return frames
 
-def save_video(output_video_frames, output_video_path):
-    if not output_video_frames:
-        print("No frames to save.")
-        return
-
+def save_video(ouput_video_frames,output_video_path):
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    height, width, _ = output_video_frames[0].shape
-    out = cv2.VideoWriter(output_video_path, fourcc, 24, (width, height))
-
-    for frame in output_video_frames:
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (ouput_video_frames[0].shape[1], ouput_video_frames[0].shape[0]))
+    for frame in ouput_video_frames:
         out.write(frame)
-
-    out.release()  # Release the video writer object
-
-
-    
+    out.release()
